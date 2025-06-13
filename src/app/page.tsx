7cacf0f002bd7b93c5afba0336a66bdaae24ef0d
geol2 <big9401@gmail.com>
2025-06-14 00:32:31 +0900

    add common.css and page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,46 @@
-export default function Home() {
-  return (
-    <div>
-      <div className="login m-3 p-3" style={{ height: "900px" }}>
-          <div id="login-container" className="flex flex-col justify-center">
-              <form id="login-form" className="m-3 p-3" method="post">
-                  <div>
-                      <div>
-                          <h1 className="jetbrains-mono-semi-bold fs-30 text-center">로그인</h1>
-                          <p className="jetbrains-mono-light text-center m-1">관리자 페이지에 오신 것을 환영합니다!</p>
-                      </div>
-                      <div className="jetbrains-mono-light mt-50">
-                          <div>
-                              <p className="inline-block w-100 fs-14">아이디</p>
-                              <label>
-                                <input type="text" name="userId" className="common-input" />
-                              </label>
-                          </div>
-                      </div>
+import React from 'react';
+import './globals.css';
+import './common/common.css';
 
-                      <div className="jetbrains-mono-light mt-1">
-                          <div>
-                              <p className="inline-block w-100 fs-14">패스워드</p>
-                              <label><input type="password" name="userPassword" className="common-input" /></label>
-                          </div>
-                      </div>
-                      <div className="flex mt-1">
-                          <label>
-                            <input type="checkbox" name="remember-me" id="remember-me" className="" />
-                          </label>
-                          <p className="flex align-center ml-02 fs-13">자동 로그인</p>
-                      </div>
-                  </div>
-                  <div style={{ marginTop: "3rem" }} >
-                      <button type="submit" className="submit im-w-100per">로그인</button>
-                  </div>
-              </form>
-          </div>
+export default function Home() {
+    return (
+    <div>
+        <div className="login m-3 p-3" style={{ height: "900px" }}>
+            <div id="login-container" className="flex flex-col justify-center">
+                <form id="login-form" className="m-3 p-3" method="post">
+                    <div>
+                        <div>
+                            <h1 className="jetbrains-mono-semi-bold fs-30 text-center">로그인</h1>
+                            <p className="jetbrains-mono-light text-center m-1">관리자 페이지에 오신 것을 환영합니다!</p>
+                        </div>
+                        <div className="jetbrains-mono-light mt-50">
+                            <div>
+                                <p className="inline-block w-100 fs-14">아이디</p>
+                                <label>
+                                    <input type="text" name="userId" className="common-input" />
+                                </label>
+                            </div>
+                        </div>
+
+                        <div className="jetbrains-mono-light mt-1">
+                            <div>
+                                <p className="inline-block w-100 fs-14">패스워드</p>
+                                <label><input type="password" name="userPassword" className="common-input" /></label>
+                            </div>
+                        </div>
+                        <div className="flex mt-1">
+                            <label>
+                                <input type="checkbox" name="remember-me" id="remember-me" className="" />
+                            </label>
+                            <p className="flex align-center ml-02 fs-13">자동 로그인</p>
+                        </div>
+                    </div>
+                    <div style={{ marginTop: "3rem" }} >
+                        <button type="submit" className="submit im-w-100per">로그인</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-  );
+    );
 }
